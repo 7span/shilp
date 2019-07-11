9962bdb0a93d34a3116fed9259a089796c98ed64
Harsh <mail@theharsh.in>
2019-07-11 16:28:31 +0530

    Form Groups | Gap Multiclass Fix

--- src/views/demos/input.vue
+++ src/views/demos/input.vue
@@ -5,24 +5,48 @@
     .block.is-4
       .field.is-loading
         label First Name
-        input(type="text" placeholder="e.g. John")
+        input.field__block(type="text" placeholder="e.g. John")
+        small This is help text.
+    .block.is-4
+      .field
+        label Last Name
+        input.field__block(type="text" placeholder="e.g. Doe")
+        small This is help text.
+
+    .block.is-4
+      .field
+        label Email
+        .field__group
+          .field__before @
+          input.field__block(type="email" placeholder="e.g. john@doe.com")
+        small This is help text.
+
+  .blocks
+    .block.is-4
+      .field.is-loading
+        label First Name
+        input.field__block(type="text" placeholder="e.g. John")
         small This is help text.
     .block.is-4
       .field.is-danger
         label Last Name
-        input(type="text" placeholder="e.g. Doe")
+        input.field__block(type="text" placeholder="e.g. Doe")
         small This is help text.
+
     .block.is-4
       .field.is-success
         label Email
-        input(type="email" placeholder="e.g. john@doe.com")
+        .field__group
+          .field__before @
+          input.field__block(type="email" placeholder="e.g. john@doe.com")
+          .field__after .com
         small This is help text.
 
   .blocks
     .block.is-4
       .field
         label City
-        select
+        select.field__block
           option Select The Option
           option Ahmedabad
           option Rajkot
